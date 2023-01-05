081ab5c6fd880fe84dcc0cd5b3def84859478d01
Aaron Jarmusch <jarmusch@udel.edu>
2023-01-05 16:35:42 -0500

    fix 1

--- static/database/index.php
+++ static/database/index.php
@@ -5,18 +5,19 @@ $conn = OpenCon();
 
 echo "Connected Successfully";
 
-$sql = "SELECT system_name FROM Results";
+$sql = "SELECT * FROM Results";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    //output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "\nname = " . $row["system_name"] . "\n";
-    }
-} else {
-    echo "0 results";
-}
-
 CloseCon($conn);
+
+// if ($result->num_rows > 0) {
+//     //output data of each row
+//     while($row = $result->fetch_assoc()) {
+//         echo "\nname = " . $row["system_name"] . "\n";
+//     }
+// } else {
+//     echo "0 results";
+// }
+
 
 ?>
